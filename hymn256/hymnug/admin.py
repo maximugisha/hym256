06c60b5f2aad7a8c52e58f6a00e2170842004a0f
maximugisha <maximugisha@gmail.com>
2021-06-08 18:36:36 +0300

    registered app and added models

--- hymn256/hymnug/admin.py
+++ hymn256/hymnug/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Language, Hymn, Ad
 # Register your models here.
+admin.site.register(Language)
+admin.site.register(Hymn)
+admin.site.register(Ad)
--- hymn256/hymnug/models.py
+++ hymn256/hymnug/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Hymn(models.Model):
+    name = models.CharField(max_length=255)
+    lyrics = models.CharField(max_length=25525)
+    url = models.CharField(max_length=255)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    created = models.DateTimeField(default=timezone.now)
+    expired = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
