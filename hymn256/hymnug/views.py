6637d00b815efde406573e48df396f1519e9c9a4
maximugisha <maximugisha@gmail.com>
2021-06-09 08:12:40 +0300

    added views and api endpoints

--- hymn256/hymnug/views.py
+++ hymn256/hymnug/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import LanguageSerializer, HymnSerializer, AdSerializer
+from .models import Language, Hymn, Ad
+
 
 # Create your views here.
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    queryset = Language.objects.all().order_by('name')
+    serializer_class = LanguageSerializer
+
+
+class HymnViewSet(viewsets.ModelViewSet):
+    queryset = Hymn.objects.all().order_by('name')
+    serializer_class = HymnSerializer
+
+
+class AdViewSet(viewsets.ModelViewSet):
+    queryset = Ad.objects.all().order_by('name')
+    serializer_class = AdSerializer
