5802c320ccbb90c99d15aab5e946b16367c46d96
maximugisha <maximugisha@gmail.com>
2021-06-09 16:57:47 +0300

    registered em in admin

--- hymnug/admin.py
+++ hymnug/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Language, Hymn, Ad
+from .models import Language, Hymn, HymnNumber, HymnFile, Ad
 # Register your models here.
 admin.site.register(Language)
 admin.site.register(Hymn)
+admin.site.register(HymnNumber)
+admin.site.register(HymnFile)
 admin.site.register(Ad)
