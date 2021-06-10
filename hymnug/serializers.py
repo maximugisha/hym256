b5a16ab4b7ef34e7af5a8069fd7dd77d23097086
maximugisha <maximugisha@gmail.com>
2021-06-10 09:55:45 +0300

    added id in serializers.py

--- hymnug/serializers.py
+++ hymnug/serializers.py
@@ -6,28 +6,28 @@ from .models import Language, Hymn, Ad, HymnNumber, HymnFile
 class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
-        fields = '__all__'
+        fields = ['id', 'name', 'short_name', 'created', 'updated']
 
 
 class HymnSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Hymn
-        fields = '__all__'
+        fields = ['id', 'hymn_number', 'language', 'title', 'lyrics', 'created', 'updated']
 
 
 class HymnNumberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HymnNumber
-        fields = '__all__'
+        fields = ['id', 'number', 'created', 'updated']
 
 
 class HymnFileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HymnFile
-        fields = '__all__'
+        fields = ['id', 'lyrics', 'url', 'created', 'updated']
 
 
 class AdSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Ad
-        fields = '__all__'
+        fields = ['id', 'name', 'url', 'image_url', 'created', 'updated', 'expired']
