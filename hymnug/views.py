80e258aedc8c802c402dd9a9bd69bcbcd6c5259a
braop <kazibwebrianpeter@gmail.com>
2021-06-13 17:40:08 +0300

    adding django filter to installed apps

--- hymnug/views.py
+++ hymnug/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import generics
+from django_filters import rest_framework as filters
 
 from .serializers import LanguageSerializer, HymnSerializer, AdSerializer, HymnNumberSerializer, HymnFileSerializer
 from .models import Language, Hymn, Ad, HymnNumber, HymnFile
@@ -21,11 +23,29 @@ class HymnFileViewSet(viewsets.ModelViewSet):
     serializer_class = HymnFileSerializer
 
 
-class HymnViewSet(viewsets.ModelViewSet):
-    queryset = Hymn.objects.all().order_by('title')
-    serializer_class = HymnSerializer
-
-
 class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all().order_by('name')
     serializer_class = AdSerializer
+
+
+#--- Filter hymns by language id ---#
+class HymnFilter(filters.FilterSet):
+    class Meta:
+        model = Hymn
+        fields = {
+            'language' : ['exact'],
+        }
+        
+class HymnViewSet(viewsets.ModelViewSet):
+    queryset = Hymn.objects.all().order_by('title')
+    serializer_class = HymnSerializer
+    lookup_field = 'language_id'
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = HymnFilter
+    
+#class HymnViewSet(generics.ListCreateAPIView):
+#    queryset = Hymn.objects.all()
+#    serializer_class = HymnSerializer
+#    lookup_field = 'language'
+#    filter_backends = (filters.DjangoFilterBackend,)
+#    filterset_class = HymnFilter
