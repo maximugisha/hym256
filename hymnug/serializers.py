96eb1520f4f0ce2ec317f07a21b91c3da4a0a0ea
braop <kazibwebrianpeter@gmail.com>
2021-06-13 16:40:35 +0300

    initial commit

--- hymnug/serializers.py
+++ hymnug/serializers.py
@@ -3,31 +3,31 @@ from rest_framework import serializers
 from .models import Language, Hymn, Ad, HymnNumber, HymnFile
 
 
-class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
         fields = ['id', 'name', 'short_name', 'created', 'updated']
 
 
-class HymnSerializer(serializers.HyperlinkedModelSerializer):
+class HymnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hymn
         fields = ['id', 'hymn_number', 'language', 'title', 'lyrics', 'created', 'updated']
 
 
-class HymnNumberSerializer(serializers.HyperlinkedModelSerializer):
+class HymnNumberSerializer(serializers.ModelSerializer):
     class Meta:
         model = HymnNumber
         fields = ['id', 'number', 'created', 'updated']
 
 
-class HymnFileSerializer(serializers.HyperlinkedModelSerializer):
+class HymnFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = HymnFile
         fields = ['id', 'lyrics', 'url', 'created', 'updated']
 
 
-class AdSerializer(serializers.HyperlinkedModelSerializer):
+class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = ['id', 'name', 'url', 'image_url', 'created', 'updated', 'expired']
